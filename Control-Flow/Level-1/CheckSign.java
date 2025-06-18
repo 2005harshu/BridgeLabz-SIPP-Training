5123970350169b6bc9241d42ce4beb29cc652f18
Harshita Das <119880035+2005harshu@users.noreply.github.com>
2025-06-18 11:35:24 +0530

    [Harshita] Add Check the sign of number

--- Control-Flow/Level-1/CheckSign.java
+++ Control-Flow/Level-1/CheckSign.java
@@ -5,13 +5,13 @@ public class CheckSign {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
-
+     // Checking if the number is positive,negative or zero
         if (number > 0)
             System.out.println("Positive");
         else if (number < 0)
             System.out.println("Negative");
         else
             System.out.println("Zero");
-    }
+    } 
 }
 
