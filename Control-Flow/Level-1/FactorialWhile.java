d463512b8f07731094f24e4c6ca3ab52d4d455cb
Harshita Das <119880035+2005harshu@users.noreply.github.com>
2025-06-18 11:41:49 +0530

    [Harshita] add  calculating factorial using whille loop

--- Control-Flow/Level-1/FactorialWhile.java
+++ Control-Flow/Level-1/FactorialWhile.java
@@ -5,7 +5,7 @@ public class FactorialWhile {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-
+// calculating factorial using while loop
         if (n >= 0) {
             long fact = 1;
             int i = 1;
