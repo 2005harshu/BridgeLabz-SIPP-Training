4e77e23deddef37b51fa505fe4eabd762f93ebce
Harshita Das <119880035+2005harshu@users.noreply.github.com>
2025-06-18 11:52:55 +0530

    [Harshita] add Sum of numbers until zero or negative

--- Control-Flow/Level-1/SumUntilZeroOrNegative.java
+++ Control-Flow/Level-1/SumUntilZeroOrNegative.java
@@ -4,7 +4,7 @@ public class SumUntilZeroOrNegative {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total = 0;
-
+// sum of numbers until zero or negative
         System.out.println("Enter numbers (0 or negative to stop): ");
         while (true) {
             double value = sc.nextDouble();
