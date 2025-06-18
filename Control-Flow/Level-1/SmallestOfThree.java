f034133ce277fe80cd74959c1b36149b5a9ef18e
Harshita Das <119880035+2005harshu@users.noreply.github.com>
2025-06-18 11:45:29 +0530

    [Harshita] add finding smallest of three

--- Control-Flow/Level-1/SmallestOfThree.java
+++ Control-Flow/Level-1/SmallestOfThree.java
@@ -4,6 +4,7 @@ public class SmallestOfThree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter 3 numbers: ");
+        //finding smallest of three
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
         System.out.println("Is the first number the smallest? " + (a < b && a < c));
     }
