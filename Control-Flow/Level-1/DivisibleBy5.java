80de92909011df54bdd3d7344c132a8098d480b1
Harshita Das <119880035+2005harshu@users.noreply.github.com>
2025-06-18 11:39:02 +0530

    [Harshita] add check if the number is divisible by 5

--- Control-Flow/Level-1/DivisibleBy5.java
+++ Control-Flow/Level-1/DivisibleBy5.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class DivisibleBy5 {
+    // check if the number is divisible by 5
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
